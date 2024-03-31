ce7adfe16d24f870b5153657c2d5ab105b2ea853
VenujaWickramaarachchi <venuja071@gmail.com>
2024-03-31 16:10:12 +0530

    nav id

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -3,7 +3,7 @@ const links = [
   { id: 2, url: 'about', text: 'about' },
   { id: 3, url: 'achievement', text: 'achievements' },
   { id: 4, url: 'contact', text: 'contact' },
-  { id: 4, url: 'gallery', text: 'gallery' },
+  { id: 5, url: 'gallery', text: 'gallery' },
 ]
 import { NavLink } from 'react-router-dom'
 
